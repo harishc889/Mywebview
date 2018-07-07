9c112798e12e4c3a84a83cf211871e35aeb0a221
manmohan chauhan <manmohanchauhan777@gmail.com>
2018-07-07 14:06:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/harish/mywebview/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.harish.mywebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView mywebview = (WebView) findViewById(R.id.webview);
+        mywebview.setWebViewClient(new MyBrowser());
+
+        mywebview.loadUrl("http://www.google.com/");
+        mywebview.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
